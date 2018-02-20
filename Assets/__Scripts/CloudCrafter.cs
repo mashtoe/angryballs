641a4745793d0d274a4cf957d555771c649e7f10
mashtoe <mashtoe@hotmail.com>
2018-02-20 12:34:29 +0100

    first

---
+++ Assets/__Scripts/CloudCrafter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour
+{
+
+    public int numClouds = 40;
+    public GameObject[] cloudPrefabs;
+    public Vector3 cloudPosMin;
+    public Vector3 cloudPosMax;
+    public float cloudScaleMin = 1;
+    public float cloudScaleMax = 5;
+    public float cloudSpeedMult = 0.5f;
+
+    public GameObject[] cloudInstances;
+
+    void Awake()
+    {
+        cloudInstances = new GameObject[numClouds];
+        GameObject anchor = GameObject.Find("CloudAnchor");
+        GameObject cloud;
+        for (int i = 0; i < numClouds; i++)
+        {
+            int prefabNum = Random.Range(0, cloudPrefabs.Length);
+            cloud = Instantiate(cloudPrefabs[prefabNum]) as GameObject;
+            Vector3 cpos = Vector3.zero;
+            cpos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cpos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+            float scaleU = Random.value;
+            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+            cpos.y = Mathf.Lerp(cloudPosMin.y, cpos.y, scaleU);
+            cpos.z = 100 - 90 * scaleU;
+            cloud.transform.position = cpos;
+            cloud.transform.localScale = Vector3.one * scaleVal;
+            cloud.transform.parent = anchor.transform;
+            cloudInstances[i] = cloud;
+
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach (GameObject cloud in cloudInstances)
+	    {
+	        float scaleVal = cloud.transform.localScale.x;
+	        Vector3 cpos = cloud.transform.position;
+	        cpos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
+	        if (cpos.x <= cloudPosMin.x)
+	        {
+	            cpos.x = cloudPosMax.x;
+	        }
+	        cloud.transform.position = cpos;
+
+
+	    }
+
+    }
+}
---
+++ Assets/__Scripts/FollowCam.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    static public FollowCam S; //singleton
+    public float easing = 0.05f;
+    public Vector2 minXY;
+
+    public GameObject poi; //point of interest
+    public float camZ;
+
+    void Awake()
+    {
+        S = this;
+        camZ = transform.position.z;
+
+    }
+
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate ()
+	{
+	    Vector3 destination;
+	    if (poi == null)
+	    {
+	        destination = Vector3.zero;
+	    }
+	    else
+	    {
+	        destination = poi.transform.position;
+	        if (poi.tag == "Projectile")
+	        {
+	            if (poi.GetComponent<Rigidbody>().IsSleeping())
+	            {
+	                poi = null;
+	                return;
+	            }
+	        }
+        }
+	    destination.x = Mathf.Max(minXY.x, destination.x);
+	    destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+	    destination.z = camZ;
+	    transform.position = destination;
+	    GetComponent<Camera>().orthographicSize = destination.y + 10;
+	}
+}
